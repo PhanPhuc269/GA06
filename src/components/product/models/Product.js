6b844abca8db6b77e1714ce32a6e99d6d322d208
mandeotv1234 <22120201@student.hcmus.edu.vn>
2024-11-28 21:42:35 +0700

    22120201 them sort

--- src/components/product/models/Product.js
+++ src/components/product/models/Product.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
-    name: {type: String, require: true},
-    description: {type: String},
-    price: {type: Number, require: true},
-    image: {type: String},
-    category: {type: String},
-    availibility: {type: String},
-    brand: {type: String},
-    type:{type: String},
-    color:{type: String}
-
+    name: { type: String, require: true },
+    description: { type: String },
+    price: { type: Number, require: true },
+    image: { type: String },
+    category: { type: String },
+    availibility: { type: String },
+    brand: { type: String },
+    type: { type: String },
+    color: { type: String },
+    rate: { type: Number }, // Sửa `Int32` thành `Number` để phù hợp với Mongoose
+    createAt: { type: Date, default: Date.now } // Thêm trường thời gian tạo
 });
+
 
 module.exports = mongoose.model('Product', Product);
