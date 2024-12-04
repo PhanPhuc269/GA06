19488b4e1fd509d9ebf4ec99fd1f9fd551110966
mandeotv1234 <22120201@student.hcmus.edu.vn>
2024-12-04 20:38:03 +0700

    Bổ sung thêm vào giỏ hàng ở trang chi tiết sản phẩm

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const db = require('./config/db');
 db.connect();
 
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3000;
 
 const route = require('./routes');  
 
